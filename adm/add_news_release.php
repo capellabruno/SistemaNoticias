1dcd43a18cc2de85d5c3deefb1229446cfe28c48
Bruno Capella <capella.bruno@gmail.com>
2014-11-06 15:41:30 -0200

    Update add_news_release.php

--- adm/add_news_release.php
+++ adm/add_news_release.php
@@ -1,4 +1,31 @@
 <?php
   include_once '../config.php';
   
+  $objDomain = new NOTICIA();
+  $obj = new NOTICIAS_NEGOC();
+  
+  $titulo="";
+  $previa ="";
+  $autor="";
+  $url_fonte="";
+  $descricao_completa="";
+  
+  
+  $titulo = url_encode($_POST['txtTitulo']);
+  $previa = url_encode($_POST['txtPrevia']);
+  $autor = url_encode($_POST['txtAutor']);
+  $url_fonte =url_encode($_POST['txtFonte']);
+  $descricao_completa = $_POST['txtDescricao'];
+
+ $objDomain->titulo=$titulo;
+ $objDomain->previa=$previa;
+ $objDomain->Autor= $autor;
+ $objDomain->Fonte_url = $url_fonte;
+ $objDomain->Descricao =$descricao_completa;
+ $objDomain->Views=0;
+ 
+ if($obj->Incluir($objDomain)>0){
+     
+ }
 ?>
+
